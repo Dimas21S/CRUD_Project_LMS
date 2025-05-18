058745fcb9cdb0673bbdd6162b7e81b9f4ae5100
Dimas Satyo Wijayanto <dewiandrianiii75@gmail.com>
2025-05-18 22:45:09 +0700

    Perbaikan DBConfig

--- DBConfig.cs
+++ DBConfig.cs
@@ -11,8 +11,8 @@ namespace CRUD_Project_LMS
         public static string dbServer = "localhost";
         public static string dbUser = "root";
         public static string dbName = "winform_db";
-        public static string dbPass = "Dimas210904";
-        public static string dbPort = "3307";
+        public static string dbPass = "";
+        public static string dbPort = "";
 
         public static string ConsStr = $"server={dbServer};user={dbUser};database={dbName};password={dbPass};port={dbPort}";
     }
